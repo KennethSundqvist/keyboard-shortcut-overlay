a7bd15fe9a6e55ca2ee50fb66fa78b4e488ce11a
Kenneth Sundqvist <eviloatmeal@gmail.com>
2013-03-11 00:44:41 +0100

    Refactor KSO Builder to use jQuery

--- kso-builder.js
+++ kso-builder.js
@@ -1,160 +1,156 @@
-;var KSO_builder = (function() {
+;var KSO_builder = (function($) {
     var w = window,
         d = w.document,
-        // Output element
-        elOutput = d.getElementById('js-output'),
-        // Input element
-        elInput = d.getElementById('js-input'),
-        // Input error message element
-        elInputError = d.getElementById('js-inputError'),
-        // Form element
-        elForm = d.getElementById('js-shortcutsConf'),
-        // Form configuration title input element
-        elConfigTitle,
-        // Form groups element
-        elGroups,
+        // Form for loading an existing config into the config editor
+        $loadForm = $('#js-configLoader'),
+        // Error messages in the config loading form will be printed in this element
+        $loadErrorMessage = $('.errorMessage', $loadForm),
+        // The generated code will be printed in this textarea element
+        $output = $('#js-configOutput'),
+        // The form for editing a config
+        $editorForm = $('#js-configEditor'),
+        // Title in the config form
+        $editorTitle,
+        // Holds all groups in the config editor form
+        $editorGroups,
         // HTML templates to render with. Use $placeholders
         templates = {
-            configTitle: {
-                type: 'div', className: 'ksoBuilder_configTitle',
-                html: '<label>Overlay title <input name="configTitle" type="text"></label>'
-            },
-            groups: {
-                type: 'div', className: 'ksoBuilder_groups'
-            },
-            group: {
-                type: 'div', className: 'ksoBuilder_group',
-                html: '<div class="ksoBuilder_group_title"><label>Group title <input name="groupTitle" type="text" value="$title"></label> <button data-trigger="deleteGroup">Delete group</button></div>' +
-                      '<table class="ksoBuilder_group_shortcuts"><thead><tr><th>Shortcuts</th><th>Description</th><td></td></tr></thead><tbody></tbody></table>'
-            },
-            shortcut: {
-                type: 'tr',
-                html: '<td><input name="keys" type="text" value="$keys"></td><td><input name="desc" type="text" value="$desc"></td><td><button data-trigger="deleteShortcut">Delete</button></td>'
-            },
-            addButton: {
-                type: 'div',
-                html: '<button data-trigger="$trigger">$text</button>'
-            },
-            submitButton: {
-                type: 'div',
-                html: '<input type="submit" value="Generate KSO">'
-            }
+            configTitle: '<div class="ksoBuilder_configTitle"><label>Overlay title <input name="configTitle" type="text"></label></div>',
+            groups: '<div class="ksoBuilder_groups"></div>',
+            group: '<div class="ksoBuilder_group">' +
+                       '<div class="ksoBuilder_group_title">' +
+                           '<label>Group title <input name="groupTitle" type="text" value="$title"></label> ' +
+                           '<button data-trigger="deleteGroup">Delete group</button>' +
+                       '</div><table class="ksoBuilder_group_shortcuts"><thead><tr><th>Shortcuts</th><th>Description</th><td></td></tr></thead><tbody></tbody></table></div>',
+            shortcut: '<tr><td><input name="keys" type="text" value="$keys"></td><td><input name="desc" type="text" value="$desc"></td><td><button data-trigger="deleteShortcut">Delete</button></td></tr>',
+            addButton: '<div><button data-trigger="$trigger">$text</button></div>',
+            submitButton: '<div><input type="submit" value="Generate KSO"></div>'
         },
-        // RegExp to extract the configuration when loading shortcuts
+        // RegExp to extract the configuration when loading a config
         loadRegExp = /\/\*BEGIN_CONFIG\*\/(.+)\/\*END_CONFIG\*\//
         
     
     function init(config) {
-        elConfigTitle = renderTemplate(templates.configTitle)
-        elGroups = renderTemplate(templates.groups)
+        $editorTitle = template('configTitle')
+        $editorGroups = template('groups')
         
-        elForm.appendChild(elConfigTitle)
-        elForm.appendChild(elGroups)
-        elForm.appendChild(renderTemplate(templates.addButton, { text: 'Add another group', trigger: 'addGroup' }))
-        elForm.appendChild(renderTemplate(templates.submitButton))
+        $editorForm.append(
+            $editorTitle,
+            $editorGroups,
+            template('addButton', { text: 'Add another group', trigger: 'addGroup' }),
+            template('submitButton')
+        )
         
-        d.getElementById('js-inputSubmit').addEventListener('click', loadConfig, false)
+        $loadForm.on('submit', function(e) {
+            var config
+            
+            e.preventDefault()
+            
+            try { config = $('textarea', e.target).val().match(loadRegExp)[1] }
+            catch (err) { $loadErrorMessage.html('Load error: Could not find the configuration.'); return }
+
+            try { config = JSON.parse(config) }
+            catch (err) { $loadErrorMessage.html('Load error: Configuration is not valid JSON.'); return }
+            
+            loadConfig(config)
+        })
         
-        elForm.addEventListener('click', function(e) {
-            var trigger = e.target.getAttribute('data-trigger')
+        $editorForm.on('click', function(e) {
+            var $target = $(e.target),
+                trigger = $target.attr('data-trigger')
+            
             if (!trigger) return
             e.preventDefault()
             
             switch (trigger) {
                 case 'addGroup': addGroup(); break
-                case 'deleteGroup': deleteGroup(e.target.parentElement.parentElement); break
-                case 'addShortcuts': addShortcuts(e.target.parentElement.parentElement.querySelector('tbody'), 5); break
-                case 'deleteShortcut': deleteShortcut(e.target.parentElement.parentElement); break
+                case 'deleteGroup': deleteGroup($target.parents('.ksoBuilder_group')); break
+                case 'addShortcuts': addShortcuts($('tbody', $target.parents('.ksoBuilder_group')), 5); break
+                case 'deleteShortcut': deleteShortcut($target.parents('tr').eq(0)); break
             }
-        }, false)
+        })
         
-        elForm.addEventListener('submit', parseForm, false)
+        $editorForm.on('submit', outputEditorForm)
         
         if (config) loadConfig(config)
         else addGroup()
     }
     
-    function renderTemplate(template, content) {
-        var el = d.createElement(template.type),
-            html = template.html,
-            key
+    function template(template, data) {
+        template = templates[template]
         
-        if (template.className) el.className = template.className
-        
-        if (html) {
-            for (key in content) {
-                if (content.hasOwnProperty(key))
-                    html = html.replace('$' + key, content[key])
-            }
-            
-            el.innerHTML = html
+        for (var key in data) {
+            if (data.hasOwnProperty(key))
+                template = template.replace('$' + key, data[key])
         }
         
-        return el
+        return $(template)
     }
     
     function addGroup(config) {
-        var group = renderTemplate(templates.group, { title: (config && config.title) || '' }),
-            shortcuts = group.querySelector('tbody'),
-            addShortcut = renderTemplate(templates.addButton, { text: 'Add more shortcuts', trigger: 'addShortcuts' })
+        var $group = template('group', { title: (config && config.title) || '' }),
+            $shortcuts = $('tbody', $group),
+            $addShortcut = template('addButton', { text: 'Add more shortcuts', trigger: 'addShortcuts' })
         
-        if (config && config.shortcuts.length) {
-            config.shortcuts.forEach(function(sc) {
-                shortcuts.appendChild(renderTemplate(templates.shortcut, { keys: sc[0], desc: sc[1] }))
+        if (config && config.shortcuts && config.shortcuts.length) {
+            $(config.shortcuts).each(function() {
+                $shortcuts.append(template('shortcut', { keys: this[0], desc: this[1] }))
             })
         }
         else {
-            shortcuts.appendChild(renderTemplate(templates.shortcut, { keys: '', desc: '' }))
+            $shortcuts.append(template('shortcut', { keys: '', desc: '' }))
         }
         
-        group.appendChild(addShortcut)
-        elGroups.appendChild(group)
+        $group
+            .append($addShortcut)
+            .appendTo($editorGroups)
     }
     
-    function deleteGroup(elGroup) {
-        var groupTitle = elGroup.querySelector('input[name=groupTitle]').value
+    function deleteGroup($group) {
+        var groupTitle = $('input[name=groupTitle]', $group).val()
         
         if (confirm('Delete the group "' + groupTitle + '"?')) {
-            elGroup.remove()
+            $group.remove()
             
-            if (d.querySelectorAll('.ksoBuilder_group').length === 0) {
+            if ($('.ksoBuilder_group').length === 0) {
                 addGroup()
             }
         }
     }
     
-    function addShortcuts(group, numberOfShortcuts) {
-        var shortcut,
+    function addShortcuts($group, numberOfShortcuts) {
+        var $shortcut,
             i = 0
         
         for ( ; i < numberOfShortcuts; i++) {
-            shortcut = renderTemplate(templates.shortcut, { keys: '', desc: '' })
-            group.appendChild(shortcut)
-            if (i === 0) shortcut.querySelector('input').focus()
+            $shortcut = template('shortcut', { keys: '', desc: '' })
+            $group.append($shortcut)
+            if (i === 0) $('input:first', $shortcut).focus()
         }
     }
     
-    function deleteShortcut(elShortcut) {
-        var elGroup = elShortcut.parentElement
+    function deleteShortcut($shortcut) {
+        var $shortcutsHolder = $shortcut.parent()
         
-        elShortcut.remove()
+        $shortcut.remove()
         
-        if (elGroup.children.length === 0) {
-            addShortcuts(elGroup, 1)
+        if ($('tr', $shortcutsHolder).length === 0) {
+            addShortcuts($shortcutsHolder, 1)
         }
     }
     
-    function parseForm(e) {
+    function outputEditorForm(e) {
         var newConfig = { groups: [] },
             group,
             shortcut
         
         e.preventDefault()
         
-        Array.prototype.forEach.call(elForm.querySelectorAll('input[type=text]'), function(input) {
-            var value = input.value
+        $('input[type=text]', $editorForm).each(function() {
+            var $input = $(this),
+                value = this.value
             
-            switch (input.getAttribute('name')) {
+            switch ($input.attr('name')) {
                 case 'configTitle':
                     newConfig.title = value
                     break
@@ -178,33 +174,17 @@
             }
         })
         
-        elOutput.value = 'KSO code before /*BEGIN_CONFIG*/' +
-                         JSON.stringify(newConfig) +
-                         '/*END_CONFIG*/ KSO code after'
+        $output.val('KSO code before /*BEGIN_CONFIG*/' + JSON.stringify(newConfig) + '/*END_CONFIG*/ KSO code after')
     }
     
     function loadConfig(config) {
-        var i
+        $editorGroups.empty()
         
-        if (config.target) {
-            try { config = elInput.value.match(loadRegExp)[1] }
-            catch (e) { elInputError.innerHTML = 'Could not find the configuration :('; return }
-
-            try { config = JSON.parse(config) }
-            catch (e) { elInputError.innerHTML = 'Configuration is not valid JSON :('; return }
-        }
-        
-        if (elGroups.children) {
-            for (i = elGroups.children.length - 1 ; i >= 0; i--) {
-                elGroups.children[i].parentElement.removeChild(elGroups.children[i])
-            }
-        }
-        
-        elConfigTitle.querySelector('input').value = config.title || ''
+        $('input', $editorTitle).val(config.title || '')
         
         if (config.groups && config.groups.length) {
-            config.groups.forEach(function(group) {
-                addGroup(group)
+            $(config.groups).each(function() {
+                addGroup(this)
             })
         }
         else {
@@ -215,4 +195,4 @@
     return {
         init: init
     }
-}());
+}(jQuery));
